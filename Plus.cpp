f1fee2074e2edb8607523887fbd8b917f30dde57
David Hunt <huntdog1541@gmail.com>
2015-06-07 22:07:19 -0500

    update files

---
+++ Plus.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David Hunt on 6/7/15.
+//
+
+#include "Plus.h"
---
+++ Plus.h
@@ -0,0 +1,19 @@
+//
+// Created by David Hunt on 6/7/15.
+//
+
+#ifndef PROJECT_PLUS_H
+#define PROJECT_PLUS_H
+
+
+class Plus {
+
+    void main(int arg, char * argv)
+    {
+        printf("Hello World");
+    }
+
+};
+
+
+#endif //PROJECT_PLUS_H
